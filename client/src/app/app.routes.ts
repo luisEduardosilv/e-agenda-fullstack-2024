24a449b2e33e4eb13e8a3fe39beedbb6bbdac3d8
Luis Eduardo <luiseduardosilva799@gmail.com>
2024-12-09 08:43:47 -0300

    Implementado CRUD de Medicos

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { RegistroComponent } from './auth/views/registro/registro.component';
+import { LoginComponent } from './auth/views/login/login.component';
+import { DashboardComponent } from './views/dashboard/dashboard.component';
+export const routes: Routes = [
+{
+path: 'registro',
+title: 'Registro de Usuário | e-Agenda',
+component: RegistroComponent,
+},
+{
+path: 'login',
 
-export const routes: Routes = [];
+Shell
+Modelos
+title: 'Login de Usuário | e-Agenda',
+component: LoginComponent,
+},
+{
+path: 'dashboard',
+title: 'Painel de Controle | e-Agenda',
+component: DashboardComponent,
+},
+];
